34041da72a3e21b8ea6ae1d811fbda8814c27d59
Maxim Akimenko <maxnet663@gmail.com>
2022-07-01 15:46:16 +0300

    builded by cmake

---
+++ surgeon_simulator/build/src/CMakeFiles/my_lib.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alah/HomeWorks/cmake_usage/surgeon_simulator/src/coordinates.cpp" "src/CMakeFiles/my_lib.dir/coordinates.cpp.o" "gcc" "src/CMakeFiles/my_lib.dir/coordinates.cpp.o.d"
+  "/home/alah/HomeWorks/cmake_usage/surgeon_simulator/src/instruments.cpp" "src/CMakeFiles/my_lib.dir/instruments.cpp.o" "gcc" "src/CMakeFiles/my_lib.dir/instruments.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ surgeon_simulator/build/src/CMakeFiles/surg_sim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/surg_sim.dir/main.cpp.o"
+  "CMakeFiles/surg_sim.dir/main.cpp.o.d"
+  "surg_sim"
+  "surg_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/surg_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
